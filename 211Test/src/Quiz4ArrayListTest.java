fd7a010f0cf6a7db2cd988bb81031d899ec1fe73
Mario Rodriguez <mrodrigu@northseattle.edu>
2021-10-14 20:52:57 -0700

    added Quiz4 for CS211

--- 211Test/src/Quiz4ArrayListTest.java
+++ 211Test/src/Quiz4ArrayListTest.java
@@ -1,30 +1,22 @@
+/* Mario Rodriguez, CS 211
+ * 980384835
+ * October 14, 2021
+ * Bellevue College
+ * Coding Quiz 4 - Arraylist
+ */
 
 import java.util.*;
 
-// This is a very simple and easy program.
-// But many people make mistake in real-world.
-// let's practice ArrayList to avoid the mistakes.
+// The reason that this program produces incorrect results is because when an element is removed from an ArrayList
+// all other elements are shifted to close any gaps left by the removal of any element; elements can also be shifted
+// when inserting a new element at the beginning or somewhere in the middle of the ArrayList.  The loop variable 'i'
+// was also  incremented at the end of the loop, and incremented in the second call to populate the 'member',
+// this index reference grew much faster than required.
 //
-// We want to create an app that creates sports teams.
-// There is a list of people who want to be a team.
-// Each team consists of two people; one captain and one member.
-// The algorithm is that we remove the first two people from the list,
-// then designates the first person as the captain and the second one becomes a member.
-// We do this until the list is empty.
-//
-// Now, suppose there are 6 in the list(a, b, c, d, e, f).
-// The expected output is
-// Captain: a Member: b
-// Captain: c Member: d
-// Captain: e Member: f
-//
-// However, the following program produces different output.
-//
-// What's wrong with the program? Correct the program to get the correct results.
-// Also use the comment lines at the top of your program to explain why
-// this program produces incorrect output.
-
-
+// To correct the program I realized that the first element is removed and the ArrayList is then adjusted to reflect
+// the change, i.e., the other elements are moved down. So, when 'a' is removed from index 0, 'b' takes it's place
+// and all other elements are adjusted accordingly. With this in hand I simply changed the i variable in the loop
+// to always be set to 0, that is to never be incremented.
 
 public class Quiz4ArrayListTest {
 
@@ -33,18 +25,18 @@ public class Quiz4ArrayListTest {
         ArrayList<Character> myArrayList = new ArrayList<>();
 
         // create a list of people
-        for (char c='a';c<='f';c++) {
+        for (char c = 'a';c <= 'h'; c++) {
             myArrayList.add(c);
         }
 
-        System.out.println(myArrayList);
-
         // remove first two people from the list to make a team. Each team consists of captain and member.
-        for (int i = 0; i < myArrayList.size(); i++){
-            char captain = myArrayList.remove(i);
-            char member = myArrayList.remove(i+1);
+        for (int i = 0; i < myArrayList.size(); i = 0){
+
+            char captain = myArrayList.remove(0);
+            char member = myArrayList.remove(0);
+
             System.out.println("Captain: "+ captain + " Member: "+member);
-            System.out.println(myArrayList);
+
         }
     }
 
